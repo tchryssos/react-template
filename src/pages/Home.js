1b71940a101a71c6771d5b9b0d44c45c9af2f82d
Troy Chryssos <troychryssos@gmail.com>
2018-12-10 17:15:30 -0500

    updating react, babel, eslint, etc

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,9 @@ import React from 'React'
 
 export default function Home() {
 	return (
-		<div>
-			hey good website
-		</div>
+		<>
+			<div>Whoa cool website</div>
+			<div>Peep this stack, react 16!</div>
+		</>
 	)
 }
