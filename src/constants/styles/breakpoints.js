399ac56782289db5bc788b3123270905be989a6d
Troy Chryssos <troychryssos@gmail.com>
2019-11-25 17:07:41 -0500

    using material breakpoints

--- src/constants/styles/breakpoints.js
+++ src/constants/styles/breakpoints.js
@@ -1,9 +1,11 @@
-export const MD_MIN_VALUE = 768
-export const LG_MIN_VALUE = 992
-export const XL_MIN_VALUE = 1200
+export const SM_MIN_VALUE = 600
+export const MD_MIN_VALUE = 960
+export const LG_MIN_VALUE = 1280
+export const XL_MIN_VALUE = 1920
 
 const mediaString = '@media'
-export const SM_MAX_STRING = `${mediaString} (max-width: ${MD_MIN_VALUE - 1}px)`
+export const XS_MAX_STRING = `${mediaString} (max-width: ${SM_MIN_VALUE - 1}px)`
+export const SM_MIN_STRING = `${mediaString} (min-width: ${SM_MIN_VALUE}px)`
 export const MD_MIN_STRING = `${mediaString} (min-width: ${MD_MIN_VALUE}px)`
 export const LG_MIN_STRING = `${mediaString} (min-width: ${LG_MIN_VALUE}px)`
 export const XL_MIN_STRING = `${mediaString} (min-width: ${XL_MIN_VALUE}px)`
