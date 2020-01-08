3b40078c148913d0f8830864ba0ea7b97e4e0203
Troy Chryssos <troychryssos@gmail.com>
2020-01-07 23:14:59 -0500

    trying to figure out why the damn bundle is so big, I think mode switching is not working properly

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
 	context: path.join(__dirname, 'src'),
@@ -16,12 +17,17 @@ module.exports = {
 		errorDetails: true,
 		reasons: true,
 	},
+	devServer: {
+		historyApiFallback: true,
+		compress: true,
+	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: path.resolve(__dirname, 'src/app.html'),
 			filename: 'index.html',
 			hash: true,
 		}),
+		// new BundleAnalyzerPlugin(),
 	],
 	optimization: {
 		splitChunks: {
