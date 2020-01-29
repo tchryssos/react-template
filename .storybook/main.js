d451730eca06f04ff293bda03bfac9b51773ed3f
Troy Chryssos <troychryssos@gmail.com>
2020-01-28 22:42:22 -0500

    using jsx and storybook

---
+++ .storybook/main.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const customFunc = require('../webpack.config.js')
+
+module.exports = {
+  stories: [
+    '../src/components/*.stories.jsx',
+    '../src/components/**/*.stories.jsx',
+  ],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  webpackFinal: async config => {
+    const custom = customFunc(null, { mode: 'staging'})
+
+    return ({
+      ...config,
+      module: {
+        ...config.module,
+        rules: [
+          ...custom.module.rules,
+        ],
+      },
+      resolve: {
+        ...config.resolve,
+        ...custom.resolve,
+        alias: {
+          components: path.join(__dirname, '../src/components'),
+          static: path.join(__dirname, '../src/static'),
+          pages: path.join(__dirname, '../src/pages'),
+          styles: path.join(__dirname, '../src/styles'),
+          constants: path.join(__dirname, '../src/constants'),
+          util: path.join(__dirname, '../src/util'),
+          contexts: path.join(__dirname, '../src/contexts'),
+        },
+      },
+    })
+  },
+};
